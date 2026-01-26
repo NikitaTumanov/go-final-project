ac5aa0e22fedaf02ebc8484ec562a55a53ab7bf7
Nikita Tumanov <toomuchick@gmail.com>
2026-01-26 21:07:36 +0300

    remove auth from nextday_handler

--- internal/pkg/api/api.go
+++ internal/pkg/api/api.go
@@ -37,7 +37,7 @@ var (
 )
 
 func Init() {
-	http.HandleFunc("/api/nextdate", auth(nextDayHandler))
+	http.HandleFunc("/api/nextdate", nextDayHandler)
 	http.HandleFunc("/api/task", auth(taskHandler))
 	http.HandleFunc("/api/tasks", auth(tasksHandler))
 	http.HandleFunc("/api/task/done", auth(doneTaskHandler))
